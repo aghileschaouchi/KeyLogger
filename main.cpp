9c9c4dd342506ddcf7e33e6682672e4c878a88e4
Aghiles Chaouchi <chaouchi.aghiles@outlook.com>
2017-12-27 16:57:56 +0100

    Masking the program

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    MSG msg;
+
+    while(GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
+}
